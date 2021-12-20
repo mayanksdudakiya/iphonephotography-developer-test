f24972edee680caec0fc5f655f03911a1d19a8bb
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-20 23:35:30 +0530

    Global test functions created for resuability in test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,82 @@
 
 namespace Tests;
 
+use App\Events\CommentWritten;
+use App\Listeners\UnlockAchievementBadgeListener;
+use App\Models\Comment;
+use App\Models\Lesson;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Wakeup data seeder for testing
+     */
+    protected function dataSeeder()
+    {
+        $this->seed();
+
+        $this->seed(AchievementsSeeder::class);
+
+        $this->seed(BadgesSeeder::class);
+    }
+
+    /**
+     * Create a user
+     * Check user authentication
+     */
+    protected function authorizedUser()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Check user authentication
+        $this->actingAs($user)->get('/login');
+
+        return $user;
+    }
+
+    /**
+     * Generate number of comments for given user
+     */
+    protected function generateComments($numberOfComments, $user)
+    {
+        for ($i = 0; $i < $numberOfComments; $i++) {
+            
+            $attributes = [
+                'body' => $this->faker->sentence,
+                'user_id' => $user->id
+            ];
+
+            // Create comment
+            $comment = Comment::factory()->create($attributes);
+
+            // Events & Listening 
+            $commentCreated = new CommentWritten($comment);
+            $listener = new UnlockAchievementBadgeListener();
+            $listener->handle($commentCreated);
+        }
+    }
+
+    /**
+     * Generate number of lessons watched for given user
+     */
+    protected function generateLessons($numberOfLessons, $user)
+    {   
+        // Get all the lessons
+        $lessons = Lesson::get();
+
+        // Attach all the lessons to the user
+        foreach ($lessons as $lesson) {
+            $user->lessons()->attach($lesson->id, ['watched' => false]);
+        }
+        
+        // Number of lessons watched by the user 
+        for ($i = 0; $i < $numberOfLessons; $i++) {
+            
+        }
+    }
 }

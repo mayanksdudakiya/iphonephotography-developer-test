e31136371cdd4986ba4b8c4e52c143140bb740af
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-21 00:08:25 +0530

    Extra code has removed from the events

--- app/Events/BadgeUnlocked.php
+++ app/Events/BadgeUnlocked.php
@@ -3,17 +3,12 @@
 namespace App\Events;
 
 use App\Models\User;
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class BadgeUnlocked
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
     public $badge_name;
     
@@ -29,15 +24,5 @@ class BadgeUnlocked
         $this->badge_name = $badge_name;
         
         $this->user = $user;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }

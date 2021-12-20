5032ef4ba1c49b0f0dd3c58d5a83787cb98de9a2
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-20 22:48:17 +0530

    Achievement table has tested with all columns using unit test

--- tests/Unit/AchievementTest.php
+++ tests/Unit/AchievementTest.php
@@ -11,10 +11,34 @@ class AchievementTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_has_a_name()
+    public function it_has_a_name_column()
     {
         $achievement = Achievement::factory()->create(['name' => 'Anonymous achievement']);
        
         $this->assertEquals('Anonymous achievement', $achievement->name);
+    }
+
+    /** @test */
+    public function it_has_a_description_column()
+    {
+        $achievement = Achievement::factory()->create(['description' => 'An achievement awarded when comment count reached one']);
+       
+        $this->assertEquals('An achievement awarded when comment count reached one', $achievement->description);
+    }
+
+    /** @test */
+    public function it_has_a_qualify_column()
+    {
+        $achievement = Achievement::factory()->create(['qualify' => 5]);
+       
+        $this->assertEquals(5, $achievement->qualify);
+    }
+
+    /** @test */
+    public function it_has_a_type_column()
+    {
+        $achievement = Achievement::factory()->create(['type' => 'comment']);
+       
+        $this->assertEquals('comment', $achievement->type);
     }
 }

289027ca17447905b9124222175100ad151c5c0d
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-20 22:45:30 +0530

    Achievement factory has added

---
+++ database/factories/AchievementFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Achievement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AchievementFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Achievement::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->firstName(),
+            'description' => $this->faker->sentence(),
+            'qualify' => $this->faker->randomElement([ 1, 5, 10, 15 ]), 
+            'type' => $this->faker->randomElement(['comment', 'lesson'])
+        ];
+    }
+}

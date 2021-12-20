25c407a68d54872059a5684fa9c8e687694db6b1
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-21 00:33:36 +0530

    Comment achievement logic with Achievement unlocked event

--- app/Traits/CommentAchievements.php
+++ app/Traits/CommentAchievements.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits;
 
+use App\Events\AchievementUnlocked;
 use App\Models\Achievement;
 
 trait CommentAchievements {
@@ -9,6 +10,26 @@ trait CommentAchievements {
     // Unlock comment achievement based on qualifier count
     public function unlockCommentAchievement()
     {
+        // Get the current user
+        $user = auth()->user();
+
+        // Comment count
+        $commentCount = $user->comments->count();
         
+        // Get the comment achievements
+        $allCommentAchievements = Achievement::commentType()->get();
+       
+        // Check comment count qualify for the achievement
+        $commentAchievement = $allCommentAchievements->first( function($achievement) use ($commentCount) {
+            return $achievement->qualify === $commentCount;
+        }); 
+
+        // Attach achievements to the user in user_achievements
+        if (!empty($commentAchievement)) :
+            $commentAchievement->awardTo($user);
+            
+            // Fire achievement unlock event
+            AchievementUnlocked::dispatch($commentAchievement->name, $user);
+        endif;
     }
 }

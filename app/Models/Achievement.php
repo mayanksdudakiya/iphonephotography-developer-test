2f5b6491cb64dc73a0680efc8f9f3ef8826dfe5c
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-21 00:11:55 +0530

    Query scope has added for comment & lesson type in achievement model

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -12,8 +12,23 @@ class Achievement extends Model
     // Allow mass assignment for now
     protected $guarded = [];
 
+    public $timestamps = true;
+
+    // Pivot relationship with user & achievement
     public function users()
     {
         return $this->belongsToMany(User::class, 'user_achievements')->withTimestamps();
+    }
+
+    // Get comment type achievement
+    public function scopeCommentType($query)
+    {
+        $query->where('type', 'comment');
+    }
+
+    // Get lesson type achievement
+    public function scopeLessonType($query)
+    {
+        $query->where('type', 'lesson');
     }
 }

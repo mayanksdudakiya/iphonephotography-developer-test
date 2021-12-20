359cc2b8daf9d84d85acdcc4e95d98fa7ecb6b03
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-21 02:22:17 +0530

    Lesson, Comment & Badge response has prepared

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -3,18 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Traits\BadgeAchievements;
+use App\Traits\CommentAchievements;
+use App\Traits\LessonAchievements;
 use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
+    use CommentAchievements, LessonAchievements, BadgeAchievements;
+
     public function index(User $user)
     {
-        return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
-        ]);
+        // Get the comment response from the trait
+        $commentResponse = $this->commentAchievementResponse();
+        
+        // Get the lesson response from the trait
+        $lessonResponse = $this->lessonAchievementResponse();
+        
+        // Merge comment & lessons's inner collection array
+        $commentLessonResponse = $commentResponse->mergeRecursive($lessonResponse);
+
+        $badgeResponse = $this->badgeResponse();
+        
+        $response = collect($commentLessonResponse)->merge($$badgeResponse);
+        
+        return response()->json($response);
     }
 }

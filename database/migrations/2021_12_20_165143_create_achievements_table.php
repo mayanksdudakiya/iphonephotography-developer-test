3615eb727ac0741d055663c0eed3ae90a1d4ec21
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-20 22:24:35 +0530

    Achievement migration and model created for unit test

---
+++ database/migrations/2021_12_20_165143_create_achievements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAchievementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->integer('qualify');
+            $table->enum('type', ['comment', 'lesson']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('achievements');
+    }
+}

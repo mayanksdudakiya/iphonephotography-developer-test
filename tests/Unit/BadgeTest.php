602679b9f49d287b4b1d96d504c7ae6235d61533
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2021-12-20 23:01:12 +0530

    Badge unit test has done with all table columns

--- tests/Unit/BadgeTest.php
+++ tests/Unit/BadgeTest.php
@@ -2,17 +2,32 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Badge;
+use Tests\TestCase;
 
 class BadgeTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    /** @test */
+    public function it_has_a_name_column()
     {
-        $this->assertTrue(true);
+        $badge = Badge::factory()->create(['name' => 'Anonymous badge']);
+       
+        $this->assertEquals('Anonymous badge', $badge->name);
+    }
+
+    /** @test */
+    public function it_has_a_description_column()
+    {
+        $badge = Badge::factory()->create(['description' => 'An badge awarded when achievement count reached one']);
+       
+        $this->assertEquals('An badge awarded when achievement count reached one', $badge->description);
+    }
+
+    /** @test */
+    public function it_has_a_qualify_column()
+    {
+        $badge = Badge::factory()->create(['qualify' => 5]);
+       
+        $this->assertEquals(5, $badge->qualify);
     }
 }
